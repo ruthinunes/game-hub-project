1a396ae62e5d322d110dcbdf95d4c6eeada551aa
ruthinunes <ruthinunesdev@gmail.com>
2023-07-21 19:09:59 -0300

    Removing API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: '116a31fe03c44cdaaf6b29fc4b493247',
+    key: import.meta.env.VITE_API_KEY_GAME,
   },
 });
