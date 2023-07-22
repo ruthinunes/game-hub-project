03dfd8e38d20b5ac11e6f34dbb3c85a16f39d1fc
ruthinunes <ruthinunesdev@gmail.com>
2023-07-21 23:05:24 -0300

    Displaying platform icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
-      </Show>
-      <GridItem area="main">
-        <GameGrid />
-      </GridItem>
-    </Grid>
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside">Aside</GridItem>
+        </Show>
+        <GridItem area="main">
+          <GameGrid />
+        </GridItem>
+      </Grid>
+      
+    </>
   );
 }
 
