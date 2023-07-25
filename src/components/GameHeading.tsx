0bdc6dc16d146d6ef3ab5221a730d0809fdce163
ruthinunes <ruthinunesdev@gmail.com>
2023-07-25 13:34:22 -0300

    Clean up the genres

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -10,7 +10,7 @@ const GameHeading = ({ gameQuery }: Props) => {
     gameQuery.genre?.name || ""
   } Game`;
 
-  return <Heading as="h1" marginY={5}>{heading}</Heading>;
+  return <Heading as="h1" marginY={5} fontSize='24px'>{heading}</Heading>;
 };
 
 export default GameHeading;
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,6 +6,7 @@ import {
   List,
   Button,
   Spinner,
+  Heading,
 } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -23,6 +24,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: selectGenreProps) => {
 
   return (
     <>
+    <Heading fontSize='2l' marginBottom={3}>Genres</Heading>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
@@ -31,12 +33,14 @@ const GenreList = ({ selectedGenre, onSelectGenre }: selectGenreProps) => {
                 src={getCroppedImageUrl(genre.image_background)}
                 boxSize="32px"
                 borderRadius={8}
+                objectFit='cover'
               />
               <Button
                 onClick={() => onSelectGenre(genre)}
                 fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
                 fontSize="lg"
                 variant="link"
+                whiteSpace='normal'textAlign='left'
               >
                 {genre.name}
               </Button>
