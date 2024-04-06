5fba7efb3c9bdb54741d8c42ec9182bfbc822102
ruthinunes <pamelanunesdev@gmail.com>
2024-04-06 18:07:50 -0300

    Update api route

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,16 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default axios.create({
+const apiClient = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: import.meta.env.VITE_API_KEY_GAME,
   },
 });
+
+apiClient.interceptors.request.use((config: AxiosRequestConfig) => {
+  config.headers["Access-Control-Allow-Origin"] =
+    "https://game-hub-project-2023-kezpf8429-ruthinunes.vercel.app";
+  return config;
+});
+
+export default apiClient;
