2d61f825bc2495e4c8734912e0fea7397cb2e866
ruthinunes <pamelanunesdev@gmail.com>
2024-04-06 18:12:09 -0300

    Updating axios headers

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -8,6 +8,7 @@ const apiClient = axios.create({
 });
 
 apiClient.interceptors.request.use((config: AxiosRequestConfig) => {
+  config.headers = config.headers || {};
   config.headers["Access-Control-Allow-Origin"] =
     "https://game-hub-project-2023-kezpf8429-ruthinunes.vercel.app";
   return config;
